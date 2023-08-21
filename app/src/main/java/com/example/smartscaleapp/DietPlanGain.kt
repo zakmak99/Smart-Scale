00ed642adfd8179759c5447427218bd261cd8c2e
Luis Bonilla <LBonilla1@student.fullsail.edu>
2023-08-21 17:43:40 -0400

    remaking the diet plan classes to fix it

---
+++ app/src/main/java/com/example/smartscaleapp/DietPlanGain.kt
@@ -0,0 +1,13 @@
+package com.example.smartscaleapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DietPlanGain : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_diet_gain)
+
+
+    }
+}
