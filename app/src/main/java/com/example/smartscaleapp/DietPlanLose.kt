14c6b7507542850fe83beed8f12a138f4847e582
Luis Bonilla <LBonilla1@student.fullsail.edu>
2023-08-24 19:25:16 -0400

    created the lose class list

--- app/src/main/java/com/example/smartscaleapp/DietPlanLose.kt
+++ app/src/main/java/com/example/smartscaleapp/DietPlanLose.kt
@@ -2,12 +2,24 @@ package com.example.smartscaleapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class DietPlanLose : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_diet_lose)
 
+        val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
 
+        val foodItemList = listOf(
+            FoodItem("Chicken and white Rice", 526, 58.4),
+            FoodItem("Salmon", 175, 22.1),
+
+            )
+
+        val adapter = DietPlanGainAdapter(foodItemList)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
