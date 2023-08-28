e5b54ad4b7dbb2d74623d64b2de8fbdcd017e387
zaine <zjnarog@student.fullsail.edu>
2023-08-28 09:31:21 -0700

    added bottom bar

--- app/src/main/java/com/example/smartscaleapp/Title.kt
+++ app/src/main/java/com/example/smartscaleapp/Title.kt
@@ -5,22 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+class Title : Fragment() {
 
-
-class NavHostFragment : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_nav_host_fragment, container, false)
-    }
 
+        return inflater.inflate(R.layout.fragment_title, container, false)
+    }
 }
+
