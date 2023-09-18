106291880f757aa678c6073e44e1c6a6bed308a8
yoendris97 <yrodriguez2@student.fullsail.edu>
2023-09-18 16:03:47 -0400

    Implemented BMI Calculator using fragments

--- app/src/main/java/com/example/smartscaleapp/BMICalculator1.kt
+++ app/src/main/java/com/example/smartscaleapp/BMICalculator1.kt
@@ -5,55 +5,97 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [BMICalculator1.newInstance] factory method to
- * create an instance of this fragment.
- */
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioGroup
+import android.content.Intent
+import android.widget.TextView
 class BMICalculator1 : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_bmi_calculator1, container, false)
+        val view = inflater.inflate(R.layout.fragment_bmi_calculator1, container, false)
+
+
+        val calculateButton = view.findViewById<Button>(R.id.calculateButton)
+        calculateButton.setOnClickListener {
+            calculateBMI(view)
+        }
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment BMICalculator1.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            BMICalculator1().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+
+
+    private fun calculateBMI(view: View) {
+        val weightEditText = view.findViewById<EditText>(R.id.weightEditText)
+        val heightEditText = view.findViewById<EditText>(R.id.heightEditText)
+        val genderRadioGroup = view.findViewById<RadioGroup>(R.id.genderRadioGroup)
+
+        val weightInput = weightEditText.text.toString()
+        val heightInput = heightEditText.text.toString()
+
+        if (weightInput.isBlank()) {
+            // Show an error message to the user weight input
+            weightEditText.error = "Please enter weight"
+            return
+        }
+
+        if (heightInput.isBlank()) {
+            // Show an error message to the user for height input
+            heightEditText.error = "Please enter height"
+            return
+        }
+
+        val weight = weightInput.toFloat()
+        val height = heightInput.toFloat() / 100 // Convert cm to m
+        val gender = if (genderRadioGroup.checkedRadioButtonId == R.id.maleRadioButton) "male" else "female"
+
+        val bmi = weight / (height * height)
+
+        val resultText = when {
+            bmi < 18.5 -> "Underweight"
+            bmi < 24.9 -> "Normal Weight"
+            bmi < 29.9 -> "Overweight"
+            else -> "Obese"
+        }
+
+//        val intent = Intent(requireActivity(), BMIResults1::class.java)
+//        intent.putExtra("bmi", bmi)
+//        intent.putExtra("result", resultText)
+//        requireActivity().startActivity(intent)
+
+
+
+        // Pass the BMI and result text as arguments to the next fragment
+        val bundle = Bundle()
+        bundle.putFloat("bmi", bmi)
+        bundle.putString("result", resultText)
+
+        val bmiResultsFragment = BMIResults1()
+        bmiResultsFragment.arguments = bundle
+
+        val transaction = requireActivity().supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragmentContainer, bmiResultsFragment)
+        transaction.addToBackStack(null)  // Optional: This allows the user to go back to the calculator
+        transaction.commit()
+
+        val calculateButton = view.findViewById<Button>(R.id.calculateButton)
+        calculateButton.visibility = View.GONE
+
+
+
     }
+
+
+
 }
